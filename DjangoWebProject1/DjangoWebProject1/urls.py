0fac2849c946e00c6320809ff6c72e7ddc6c7c86
kaizermd <kaizer-md@mail.ru>
2017-10-15 23:50:19 +0300

    add website

---
+++ DjangoWebProject1/DjangoWebProject1/urls.py
@@ -0,0 +1,76 @@
+﻿"""
+Definition of urls for DjangoWebProject1.
+"""
+
+from datetime import datetime
+#from django.conf.urls import patterns, url
+#from app.forms import BootstrapAuthenticationForm
+
+from django.conf.urls import include, url
+from django.contrib import admin
+admin.autodiscover()
+
+from app.forms import BootstrapAuthenticationForm
+from app.views import home, contact, about, artists, artistsdetails
+from django.contrib.auth.views import login, logout
+# Uncomment the next lines to enable the admin:
+# from django.conf.urls import include
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    url(r'^artists$', artists, name='artists'), 
+    url(r'^artists/(?P<name>[A-Za-z]+)$', artistsdetails, name='artistsdetails'),
+    url(r'^$', home, name='home'),
+    url(r'^contact$', contact, name='contact'),
+    url(r'^about', about, name='about'),
+    url(r'^login/$',
+        login,
+        {
+            'template_name': 'app/login.html',
+            'authentication_form': BootstrapAuthenticationForm,
+            'extra_context':
+            {
+                'title':'Log in',
+                'year':datetime.now().year,
+            }
+        },
+        name='login'),
+    url(r'^logout$',
+        logout,
+        {
+            'next_page': '/',
+        },
+        name='logout')
+    ]
+
+#urlpatterns = patterns('',
+#    # Examples:
+#    url(r'^$', 'app.views.home', name='home'),
+#    url(r'^contact$', 'app.views.contact', name='contact'),
+#    url(r'^about', 'app.views.about', name='about'),
+#    url(r'^login/$',
+#        'django.contrib.auth.views.login',
+#        {
+#            'template_name': 'app/login.html',
+#            'authentication_form': BootstrapAuthenticationForm,
+#            'extra_context':
+#            {
+#                'title':'Log in',
+#                'year':datetime.now().year,
+#            }
+#        },
+#        name='login'),
+#    url(r'^logout$',
+#        'django.contrib.auth.views.logout',
+#        {
+#            'next_page': '/',
+#        },
+#        name='logout'),
+
+#    # Uncomment the admin/doc line below to enable admin documentation:
+#    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+#    # Uncomment the next line to enable the admin:
+#    # url(r'^admin/', include(admin.site.urls)),
+#)
---
+++ DjangoWebProject1/app/views.py
@@ -0,0 +1,73 @@
+﻿"""
+Definition of views.
+"""
+
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.template import RequestContext, loader
+from datetime import datetime
+
+def artists(request):
+    return HttpResponse('<html><head><title>Hello, Django!</title></head><body><h1>Hello, Django!</h1></body></html>')
+
+def artistsdetails(request, name):
+    output = '<html><head><title>'+name
+    output += '</title></head><body><h1>' +name
+    output += '</h1></body></html>'
+    return HttpResponse(output)
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    context_instance = {
+            'title':'Home Page',
+            'year':datetime.now().year,
+        }
+    t = loader.get_template('app/index.html')
+    return render(
+        request,
+        'app/index.html', context_instance
+        #context_instance = RequestContext(request,
+        #{
+        #    'title':'Home Page',
+        #    'year':datetime.now().year,
+        #})
+    )
+
+def contact(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    context_instance = {
+            'title':'Contact',
+            'message':'Your contact page.',
+            'year':datetime.now().year,
+        }
+    return render(
+        request,
+        'app/contact.html',context_instance
+        #context_instance = RequestContext(request,
+        #{
+        #    'title':'Contact',
+        #    'message':'Your contact page.',
+        #    'year':datetime.now().year,
+        #})
+    )
+
+def about(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    context_instance = {
+            'title':'About',
+            'message':'Your application description page.',
+            'year':datetime.now().year,
+        }
+    return render(
+        request,
+        'app/about.html', context_instance
+        #context_instance = RequestContext(request,
+        #{
+        #    'title':'About',
+        #    'message':'Your application description page.',
+        #    'year':datetime.now().year,
+        #})
+    )
